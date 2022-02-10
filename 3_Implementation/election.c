124abd0f709414dab7d0cf1f1a40af51d262e700
yochitha41 <yochithakotha@gmail.com>
2022-02-10 21:09:48 +0530

    miniproject

--- 3_Implementation/election.c
+++ 3_Implementation/election.c
@@ -1,3 +1,13 @@
+/**
+ * @file election.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
